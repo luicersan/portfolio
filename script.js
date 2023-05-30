3ae19bdf56d84efb792e9262436d683661333121
Luis Cerrato <luicersan@alum.us.es>
2023-05-30 23:48:48 +0200

    fixed script.js

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ const toggleSwitch = document.getElementById('modo-noche-toggle');
 toggleSwitch.addEventListener('change', function () {
     document.body.classList.toggle('modo-noche');
 });
-
-const switchToggle = document.getElementById('modo-noche-toggle');
-const switchIcon = document.getElementById('modo-noche-icon');
-
-switchToggle.addEventListener('change', function () {
-    if (this.checked) {
-        switchIcon.classList.add('modo-noche');
-    } else {
-        switchIcon.classList.remove('modo-noche');
-    }
-});
